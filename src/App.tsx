16cdb980fba690542e65ff1ad7ab3e3b6a2b4c24
David Ibarra <ibarrarojashector@gmail.com>
2024-02-16 11:18:36 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ProductsList from "./components/ProductsList";
 
-function App() {
+interface Props {
+  title?: string;
+  datos?: string;
+}
+export function App({ title }: Props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ProductsList />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import carrito from "../img/carrito.png";
+import logo from "../img/logo.png";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-primary">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          <img
+            src={logo}
+            alt="logo"
+            className="mx-4"
+            style={{ width: "4rem" }}
+          />
+        </a>
+        <a href="/" className="navbar-brand">
+          Productos
+        </a>
+        <a href="/" className="navbar-brand">
+          About
+        </a>
+        <div>
+          <img
+            src={carrito}
+            alt="carrito"
+            className="mx-4 "
+            style={{ width: "4rem" }}
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
